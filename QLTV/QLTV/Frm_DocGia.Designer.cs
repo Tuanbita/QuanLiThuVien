7b4b31cbf81a4132c5ccc6c57bd467bab2818d17
lethitrang <lovely.eyes1803@gmail.com>
2016-11-23 23:27:41 +0700

    phần độc giả

---
+++ QLTV/QLTV/Frm_DocGia.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace QLTV
+{
+    partial class Frm_DocGia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTimLop = new System.Windows.Forms.TextBox();
+            this.txtTimTen = new System.Windows.Forms.TextBox();
+            this.txtTimMa = new System.Windows.Forms.TextBox();
+            this.dgvDocGia = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clmMaHS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmHoten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmNgaySinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmGT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmDiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCMND = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmLớp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmSDT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnLammoi = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocGia)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtTimLop);
+            this.groupBox2.Controls.Add(this.txtTimTen);
+            this.groupBox2.Controls.Add(this.txtTimMa);
+            this.groupBox2.Location = new System.Drawing.Point(6, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(981, 44);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tìm Kiếm";
+            // 
+            // txtTimLop
+            // 
+            this.txtTimLop.Location = new System.Drawing.Point(481, 19);
+            this.txtTimLop.Name = "txtTimLop";
+            this.txtTimLop.Size = new System.Drawing.Size(145, 20);
+            this.txtTimLop.TabIndex = 1;
+            this.txtTimLop.Text = " -- Text";
+            this.txtTimLop.Click += new System.EventHandler(this.txtTimLop_Click);
+            this.txtTimLop.TextChanged += new System.EventHandler(this.txtTimLop_TextChanged);
+            // 
+            // txtTimTen
+            // 
+            this.txtTimTen.Location = new System.Drawing.Point(131, 21);
+            this.txtTimTen.Name = "txtTimTen";
+            this.txtTimTen.Size = new System.Drawing.Size(110, 20);
+            this.txtTimTen.TabIndex = 0;
+            this.txtTimTen.Text = "   -- Họ tên học sinh";
+            this.txtTimTen.Click += new System.EventHandler(this.txtTimTen_Click);
+            this.txtTimTen.TextChanged += new System.EventHandler(this.txtTimTen_TextChanged);
+            // 
+            // txtTimMa
+            // 
+            this.txtTimMa.Location = new System.Drawing.Point(38, 21);
+            this.txtTimMa.Name = "txtTimMa";
+            this.txtTimMa.Size = new System.Drawing.Size(84, 20);
+            this.txtTimMa.TabIndex = 0;
+            this.txtTimMa.Text = "  -- Mã học sinh";
+            this.txtTimMa.Click += new System.EventHandler(this.txtTimMa_Click);
+            this.txtTimMa.TextChanged += new System.EventHandler(this.txtTimMa_TextChanged);
+            // 
+            // dgvDocGia
+            // 
+            this.dgvDocGia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDocGia.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmMaHS,
+            this.clmHoten,
+            this.clmNgaySinh,
+            this.clmGT,
+            this.clmDiaChi,
+            this.clmCMND,
+            this.clmLớp,
+            this.clmSDT});
+            this.dgvDocGia.Location = new System.Drawing.Point(0, 3);
+            this.dgvDocGia.Name = "dgvDocGia";
+            this.dgvDocGia.Size = new System.Drawing.Size(722, 207);
+            this.dgvDocGia.TabIndex = 2;
+            this.dgvDocGia.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDocGia_CellClick);
+            this.dgvDocGia.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDocGia_CellDoubleClick);
+            this.dgvDocGia.SelectionChanged += new System.EventHandler(this.dgvDocGia_SelectionChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnXoa);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.btnLammoi);
+            this.groupBox1.Controls.Add(this.btnSua);
+            this.groupBox1.Controls.Add(this.btnThem);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(731, 117);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chức năng";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgvDocGia);
+            this.panel1.Location = new System.Drawing.Point(0, 117);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(727, 213);
+            this.panel1.TabIndex = 5;
+            // 
+            // clmMaHS
+            // 
+            this.clmMaHS.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmMaHS.DataPropertyName = "maBD";
+            this.clmMaHS.HeaderText = "Mã Độc giả";
+            this.clmMaHS.Name = "clmMaHS";
+            // 
+            // clmHoten
+            // 
+            this.clmHoten.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmHoten.DataPropertyName = "hoten";
+            this.clmHoten.HeaderText = "Họ Và Tên";
+            this.clmHoten.Name = "clmHoten";
+            // 
+            // clmNgaySinh
+            // 
+            this.clmNgaySinh.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmNgaySinh.DataPropertyName = "ngaysinh";
+            this.clmNgaySinh.HeaderText = "Ngày Sinh";
+            this.clmNgaySinh.Name = "clmNgaySinh";
+            // 
+            // clmGT
+            // 
+            this.clmGT.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmGT.DataPropertyName = "GioiTinh";
+            this.clmGT.HeaderText = "Giới Tính";
+            this.clmGT.Name = "clmGT";
+            // 
+            // clmDiaChi
+            // 
+            this.clmDiaChi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmDiaChi.DataPropertyName = "diachi";
+            this.clmDiaChi.HeaderText = "Địa Chỉ";
+            this.clmDiaChi.Name = "clmDiaChi";
+            // 
+            // clmCMND
+            // 
+            this.clmCMND.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmCMND.DataPropertyName = "CMND";
+            this.clmCMND.HeaderText = "CMND";
+            this.clmCMND.Name = "clmCMND";
+            // 
+            // clmLớp
+            // 
+            this.clmLớp.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmLớp.DataPropertyName = "tenLop";
+            this.clmLớp.HeaderText = "Lớp";
+            this.clmLớp.Name = "clmLớp";
+            // 
+            // clmSDT
+            // 
+            this.clmSDT.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmSDT.DataPropertyName = "dienthoai";
+            this.clmSDT.HeaderText = "Số ĐT";
+            this.clmSDT.Name = "clmSDT";
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnXoa.Image = global::QLTV.Properties.Resources.Delete;
+            this.btnXoa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnXoa.Location = new System.Drawing.Point(487, 19);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 42);
+            this.btnXoa.TabIndex = 0;
+            this.btnXoa.Text = "Delete";
+            this.btnXoa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnXoa.UseVisualStyleBackColor = false;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnLammoi
+            // 
+            this.btnLammoi.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLammoi.Image = global::QLTV.Properties.Resources.Button_Refresh_icon__1_;
+            this.btnLammoi.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLammoi.Location = new System.Drawing.Point(326, 19);
+            this.btnLammoi.Name = "btnLammoi";
+            this.btnLammoi.Size = new System.Drawing.Size(93, 42);
+            this.btnLammoi.TabIndex = 0;
+            this.btnLammoi.Text = "Refresh";
+            this.btnLammoi.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLammoi.UseVisualStyleBackColor = false;
+            this.btnLammoi.Click += new System.EventHandler(this.btnLammoi_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSua.Image = global::QLTV.Properties.Resources.edit_icon__1_;
+            this.btnSua.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSua.Location = new System.Drawing.Point(185, 19);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(83, 42);
+            this.btnSua.TabIndex = 0;
+            this.btnSua.Text = "Edit";
+            this.btnSua.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSua.UseVisualStyleBackColor = false;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnThem.Image = global::QLTV.Properties.Resources.Actions_list_add_icon__1_;
+            this.btnThem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThem.Location = new System.Drawing.Point(44, 19);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 42);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Add";
+            this.btnThem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // Frm_DocGia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 331);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Frm_DocGia";
+            this.Text = "Frm_DocGia";
+            this.Load += new System.EventHandler(this.Frm_DocGia_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocGia)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtTimTen;
+        private System.Windows.Forms.TextBox txtTimMa;
+        private System.Windows.Forms.DataGridView dgvDocGia;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnLammoi;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtTimLop;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmMaHS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmHoten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNgaySinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmGT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmDiaChi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmCMND;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmLớp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmSDT;
+    }
+}
